283e27d5485c3bf645f66fc95bdcb5a0ed2be1d3
LiuShifeng <liu_shifeng@126.com>
2015-08-25 10:10:08 +0800

    topKHitRatio, Recall for the output file from dsgd.py

--- mf.py
+++ mf.py
@@ -22,16 +22,23 @@ except:
     steps : the maximum number of steps to perform the optimisation
     alpha : the learning rate
     beta  : the regularization parameter
+    Wm    : the training weight for unobserved rates when used in topK recommendation
 @OUTPUT:
     the final matrices P and Q
 """
-def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02, Wm = 0.0):
     Q = Q.T
     for step in xrange(steps):
         for i in xrange(len(R)):
             for j in xrange(len(R[i])):
-                if R[i][j] > 0:
-                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                if float(R[i][j]) == 0.0:
+                    for k in xrange(K):
+                        eij = Wm * eij
+                        oldPik = P[i][k]
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * oldPik - beta * Q[k][j])
+                else:
                     for k in xrange(K):
                         oldPik = P[i][k]
                         P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
@@ -40,10 +47,12 @@ def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
         e = 0
         for i in xrange(len(R)):
             for j in xrange(len(R[i])):
-                if R[i][j] > 0:
-                    e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+                if float(R[i][j]) == 0.0:
+                    e += Wm * pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+                else:
+                    e += pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
                     for k in xrange(K):
-                        e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
+                        e += (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
         if e < 0.001:
             break
     print 'R='
@@ -63,7 +72,7 @@ def RMSE(R,P,Q):
         for j in range(M):
             if R[i][j] != 0.0:
                 rmse += pow(R[i][j] - numpy.dot(P[i,:],Q[j,:]), 2)
-    return math.sqrt(rmse)
+    return math.sqrt(rmse/N)
 
 def topK_Hit_Ratio( R,users, items,K=5,relevent_bench=5):
         Hk = 0.0
@@ -80,6 +89,7 @@ def topK_Hit_Ratio( R,users, items,K=5,relevent_bench=5):
             uNu = 0
             uNku = 0
             for j in range(M):
+                print users[i,:].shape, items[j,:].shape
                 u.append(numpy.dot(users[i,:],items[j,:]))
             u.sort(reverse = True)
             for j in range(M):
@@ -113,7 +123,7 @@ if __name__ == "__main__":
 
     N = len(R)
     M = len(R[0])
-    K = 2
+    K = 3
 
     P = numpy.random.rand(N,K)
     Q = numpy.random.rand(M,K)
